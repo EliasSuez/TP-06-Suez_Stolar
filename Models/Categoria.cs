b514ec595609ca5cace71a1c971c733b1761d549
47872151 <48240167@est.ort.edu.ar>
2024-08-22 21:11:05 -0300

    correccion en Dificultad y Categoria

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,7 +1,7 @@
 public class Categoria{
     public int IdCategoria { get; private set; }
 
-    public List<string> Nombre { get; set; }
+    public static List<string> Nombre { get; set; }
 
     public string Foto {get; set;}
 
